d25c03b85e161186ff1747aa0ad20731c5540e97
Kusekwa <thatkidriroo@users.noreply.github.com>
2026-05-29 11:52:54 +0300

    Update domain to blog.kusekwa.space

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog-of-kusekwa.vercel.app',
+	site: 'https://blog.kusekwa.space',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
